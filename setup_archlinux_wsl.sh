961319df992f3547e97f8be3b6f8f6659a1c4515
tle130475c <tle130475c@gmail.com>
2026-02-20 14:12:37 +0700

    change every comment first character to uppercase

--- setup_archlinux_wsl.sh
+++ setup_archlinux_wsl.sh
@@ -8,45 +8,45 @@ username="<username>"
 realname="<realname>"
 userpass="<userpass>"
 
-# core tools
+# Core tools
 pacman -Syu --needed --noconfirm bash-completion sudo base-devel fuse2 xdg-utils
 
-# time zone
+# Time zone
 ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime
 
-# localization
+# Localization
 linum=$(sed -n '/^#en_US.UTF-8 UTF-8  $/=' /etc/locale.gen)
 sed -i "${linum}s/^#//" /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 
-# root password
+# Root password
 echo -e "${rootpass}\n${rootpass}" | passwd
 
-# add new user
+# Add new user
 useradd -G wheel,audio,lp,optical,storage,disk,video,power,render -s /bin/bash -m $username -d /home/$username -c "$realname"
 echo -e "${userpass}\n${userpass}" | passwd $username
 
-# allow user in wheel group execute any command
+# Allow user in wheel group execute any command
 linum=$(sed -n "/^# %wheel ALL=(ALL:ALL) ALL$/=" /etc/sudoers)
 sed -i "${linum}s/^# //" /etc/sudoers
 
 # Reduce the number of times re-enter password using sudo
 sed -i "$(sed -n "/^# Defaults\!.*/=" /etc/sudoers | tail -1) a Defaults timestamp_timeout=20" /etc/sudoers
 
-# core programming tools
+# Core programming tools
 pacman -Syu --needed --noconfirm git github-cli
 
-# java
+# Java
 pacman -Syu --needed --noconfirm jdk21-openjdk maven
 
-# javascript
+# Javascript
 pacman -Syu --needed --noconfirm eslint prettier nvm
 
-# python
+# Python
 pacman -Syu --needed --noconfirm uv
 
-# docker
+# Docker
 pacman -Syu --needed --noconfirm docker docker-compose docker-buildx minikube kubectl helm
 systemctl disable --now systemd-networkd-wait-online.service
 systemctl enable docker.service
